3cd0c98b010a2c5508ef8ab24ba1603ffab0c407
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-10-18 18:54:58 -0500

    Added array of questions for user input

--- index.js
+++ index.js
@@ -3,13 +3,58 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    question01 = { 
+        type: 'input',
+        name: 'title',
+        message: 'Provide a title for your project.',
+    },
+    question02 = {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project. Include the what, why, and how of your project.',
+    },
+    question03 = {
+        type: 'input',
+        name: 'install',
+        message: 'If applicable, provide a description of the steps required to install and run the application/project.',
+    },
+    question04 = {
+        type: 'input',
+        name: 'use',
+        message: 'Provide instructions for use.',
+    },    
+    question05 = {
+        type: 'input',
+        name: 'contribute',
+        message: 'If applicable, explain how other developers can contribute to the project.',
+    },
+    question06 = {
+        type: 'input',
+        name: 'test',
+        message: 'Provide instructions for testing your project.',
+    },
+    question07 = {
+        type: 'input',
+        name: 'credits',
+        message: 'List any collaborators, third-party assets, or tutorials used.',
+    },
+    question08 = {
+        type: 'input',
+        name: 'license',
+        message: 'Provide the license for the project.',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+};
 
 // Function call to initialize app
 init();
